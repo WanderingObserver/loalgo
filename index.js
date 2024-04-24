0c59edbc722ea092fd3476a4d018fc9cf74aa1f2
Mohammed Chisti <93mchisti@gmail.com>
2024-04-24 12:10:51 -0400

    Complete rehaul of the initial index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.export = {
-  intro: "You've successfully accessed LoAlgo."
+const algo = () => {
+  return {
+    intro: "Welcome to Lo Algorithm"
+  }
 }
+
+const search = () => {
+  return "Search"
+}
+
+exports.algo = algo
+exports.search = search
